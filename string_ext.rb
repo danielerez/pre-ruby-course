69faabb5f1d35e00ac5fe87d504c4f8e99d19580
Daniel Erez <derez@redhat.com>
2016-02-27 12:30:44 +0200

    implement StringExt

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,14 @@
 class StringExt
+  def initialize(first_str, second_str)
+    @first_str = first_str
+    @second_str = second_str
+  end
+
+  def concat()
+    @first_str + @second_str
+  end
+
+  def substract()
+    @first_str.gsub(@second_str, '')
+  end
 end
